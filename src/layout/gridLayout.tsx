3738010d0db023dd4f2b4123f59569b1dafa509a
Cuong <ffgcuong@gmail.com>
2023-03-01 15:57:18 +0700

    update

--- src/layout/gridLayout.tsx
+++ src/layout/gridLayout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const GridLayout = ({ children }: any) => {
     return (
         <>
-            <Grid p={30}>{children}</Grid>
+            <Grid p={30}>{children} 11</Grid>
         </>
     );
 };
