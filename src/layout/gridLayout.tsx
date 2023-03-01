e26333448bc8c976f8ecc2efd64b890201f1c377
Cuong <ffgcuong@gmail.com>
2023-03-01 15:52:53 +0700

    first

--- src/layout/gridLayout.tsx
+++ src/layout/gridLayout.tsx
@@ -1,19 +1,10 @@
 import { Grid } from "@mantine/core";
-import React, { ReactNode } from "react";
+import React from "react";
 
 const GridLayout = ({ children }: any) => {
     return (
         <>
-            <Grid p={30}>
-                {children}
-                {/* {children.map((item, index) => {
-                    return (
-                        <Grid.Col md={4} lg={3} sm={6} key={index}>
-                            {item}
-                        </Grid.Col>
-                    );
-                })} */}
-            </Grid>
+            <Grid p={30}>{children}</Grid>
         </>
     );
 };
